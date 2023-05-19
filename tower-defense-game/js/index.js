11acdf7f109516a42a4b6de9f5bb279b80238573
Sangqovin <130135413+Sangqovin@users.noreply.github.com>
2023-05-19 20:27:35 +0700

    Add files via upload

---
+++ tower-defense-game/js/index.js
@@ -0,0 +1,185 @@
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
+canvas.width = 1280
+canvas.height = 768
+	
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const placementTilesData2D = []
+
+for (let i = 0; i < placementTilesData.length; i += 20) {
+	placementTilesData2D.push(placementTilesData.slice(i, i + 20))
+}
+
+
+const placementTiles = []
+
+placementTilesData2D.forEach((row, y) => {
+	row.forEach((symbol, x) => {
+		if (symbol === 14) {
+			//add building placement tile here
+			placementTiles.push(
+				new PlacementTile({
+					position: {
+						x: x * 64,
+						y: y * 64
+					}
+				})
+			)
+		}
+	})
+})
+
+const image = new Image()
+
+image.onload = () => {
+	animate()
+}
+image.src = 'img/gameMap2.0.png'
+
+const enemies = []
+
+function spawnEnemies(spawnCount) {
+	for (let i = 1; i < spawnCount + 1; i++) {
+		const xOffset = i * 150
+		enemies.push(
+			new Enemy({
+				position: { x: waypoints[0].x - xOffset, y: waypoints[0].y}
+			})
+		)
+	}
+}
+
+
+const buildings = []
+let activeTile = undefined
+let enemyCount = 3
+let hearts = 10
+let coins = 180
+const explosions = []
+spawnEnemies(enemyCount)
+
+function animate() {
+	const animationId = requestAnimationFrame(animate)
+	c.drawImage(image, 0, 0)
+
+	for (let i = enemies.length - 1; i >= 0; i--) {
+		const enemy = enemies[i]
+			enemy.update()
+
+		if (enemy.position.x > canvas.width) {
+				hearts -= 1
+				enemies.splice(i, 1)
+				document.querySelector('#hearts').innerHTML = hearts
+				if (hearts === 0) {
+					console.log('Game Over')
+					cancelAnimationFrame(animationId)
+					document.querySelector('#gameOver').style.display = 'flex'
+				}
+		}
+	}
+
+	for (let i = explosions.length - 1; i >= 0; i--) {
+		const explosion = explosions[i]
+		explosion.draw()
+		explosion.update()
+
+		if (explosion.frames.current >= explosion.frames.max - 1) {
+			explosions.splice(i, 1)
+		}
+		console.log(explosions)
+	}
+
+	//tracking total amout of enemies
+	if (enemies.length === 0) {
+		enemyCount += 2
+		spawnEnemies(enemyCount)
+	}
+
+	placementTiles.forEach(tile => {
+		tile.update(mouse)
+	})
+
+	buildings.forEach((building) => {
+		building.update()
+		building.target = null
+		const validEnemies = enemies.filter(enemy => {
+			const xDifferent = enemy.center.x - building.center.x
+			const yDifferent = enemy.center.y - building.center.y
+			const distance = Math.hypot(xDifferent, yDifferent)
+			return distance < enemy.radius + building.radius
+		})
+		building.target = validEnemies[0]
+
+		for (let i = building.projectiles.length - 1; i >= 0; i--) {
+			const projectile = building.projectiles[i]		
+			projectile.update()
+	
+			const xDifferent = projectile.enemy.center.x - projectile.position.x
+			const yDifferent = projectile.enemy.center.y - projectile.position.y
+			const distance = Math.hypot(xDifferent, yDifferent)
+			// this is when a projectile hits a enemy
+			if (distance < projectile.enemy.radius + projectile.radius) {
+				//enemy health and enemy removal
+				projectile.enemy.health -= 20
+				if (projectile.enemy.health <= 0) {
+					const enemyIndex = enemies.findIndex((enemy) => {
+						return projectile.enemy === enemy
+					})
+					if (enemyIndex > -1) {
+						enemies.splice(enemyIndex, 1)
+						coins += 30
+						document.querySelector('#coins').innerHTML = coins
+					}
+				}
+				explosions.push(new Sprite({ 
+					position: { x: projectile.position.x, y: projectile.position.y }, 
+					imageSrc: './img/explosion.png', 
+					frames:{ max: 4 }, 
+					offset: { x: 0, y: 0 } }))
+				building.projectiles.splice(i, 1)
+			}
+		}
+	})
+}
+
+const mouse = {
+	x: undefined,
+	y: undefined
+}
+
+canvas.addEventListener('click', (event) => {
+	if (activeTile && !activeTile.isOccupied && coins - 50 >= 0) {
+		coins -= 50
+		document.querySelector('#coins').innerHTML = coins
+		buildings.push(new Building({
+			position: {
+				x: activeTile.position.x,
+				y: activeTile.position.y
+			}
+		})
+		)
+		activeTile.isOccupied = true
+		buildings.sort((a, b) => {
+			return a.position - b.position.y
+		})
+	}
+})
+
+window.addEventListener('mousemove', (event) => {
+	mouse.x = event.clientX
+	mouse.y = event.clientY
+
+	activeTile = null
+	for (let i = 0; i < placementTiles.length; i++) {
+		const tile = placementTiles[i]
+		if (mouse.x > tile.position.x && 
+			mouse.x < tile.position.x + tile.size && 
+			mouse.y > tile.position.y && 
+			mouse.y < tile.position.y + tile.size) {
+			activeTile = tile 
+		break
+	}}
+})
